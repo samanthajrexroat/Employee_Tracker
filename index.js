3197b01495a01a78ff457b9318a21aa18c969c8f
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-03-16 08:59:55 -0600

    Added main menu to the main index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Inquirer prompts
+const inquirer = require("inquirer");
+
+const menu = [
+    {
+        type: "list",
+        message: "What would you like to do?",
+        name: "options",
+        choices: 
+        [   "View All Employees", 
+            "Add Employee", 
+            "Update Employee Role", 
+            "View All Roles",
+            "Add Role", 
+            "View All Departments", 
+            "Add Department", 
+            "Exit Program"
+        ],
+    }
+];
